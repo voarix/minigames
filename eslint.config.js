c61d2088bceb5c5d17abdae52abb8e25a16fbb1d
Kachkynov Alikhan <t8t228@gmail.com>
2026-09-19 13:40:10 +0600

    chore: configure Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,32 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import unicorn from 'eslint-plugin-unicorn';
-import tseslint from 'typescript-eslint';
-import {defineConfig, globalIgnores} from 'eslint/config';
+import js from "@eslint/js";
+import globals from "globals";
+import unicorn from "eslint-plugin-unicorn";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist/**']),
+  globalIgnores(["dist/**"]),
   {
     linterOptions: {
       noInlineConfig: true,
     },
   },
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: {
       js,
       unicorn,
     },
-    extends: [
-      'js/recommended',
-      'unicorn/recommended',
-    ],
+    extends: ["js/recommended", "unicorn/recommended"],
     languageOptions: {
       globals: globals.browser,
     },
   },
   ...tseslint.configs.recommended,
   {
-    files: ['**/*.{ts,mts,cts}'],
+    files: ["**/*.{ts,mts,cts}"],
     rules: {
-      '@typescript-eslint/no-explicit-any': 'error',
+      "@typescript-eslint/no-explicit-any": "error",
     },
   },
 ]);
--- src/pages/home/home-page.ts
+++ src/pages/home/home-page.ts
@@ -1,10 +1,10 @@
 export const createHomePage = (): HTMLElement => {
-  const main = document.createElement('main');
-  const title = document.createElement('h1');
+  const main = document.createElement("main");
+  const title = document.createElement("h1");
 
-  title.textContent = 'MiniGames';
+  title.textContent = "MiniGames";
 
   main.append(title);
 
   return main;
-}
+};
