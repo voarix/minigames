b7f8d333a8590d02960ac7683a9eff960c44e0a9
Kachkynov Alikhan <t8t228@gmail.com>
2026-09-21 02:49:56 +0600

    feat: add leaderboard table section

--- src/pages/home/home-page.ts
+++ src/pages/home/home-page.ts
@@ -1,12 +1,14 @@
 import { createCarousel } from "../../components/carousel/carousel.ts";
 import { createHero } from "../../components/hero/hero.ts";
+import { createLeaderboard } from "../../components/leaderboard/leaderboard.ts";
 
 export const createHomePage = (): HTMLElement => {
   const main = document.createElement("main");
   const hero = createHero();
   const carousel = createCarousel();
+  const leaderboard = createLeaderboard();
 
-  main.append(hero, carousel);
+  main.append(hero, carousel, leaderboard);
 
   return main;
 };
